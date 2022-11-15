76651ec98cc68830fa4c7185e10ecba1c2a414cd
fpertru695 <118285546+fpertru695@users.noreply.github.com>
2022-11-15 20:02:48 +0100

    Update readme.md

--- tema3/ejercicio1.php
+++ tema3/ejercicio1.php
@@ -1,4 +1,8 @@
 <?php
+//Ejercicio 1. Función cabecera_apertura_html con un parámetro de entrada
+// y devolverá un parámetro de salida.
+// y función cierre_html() sin parámetros de entrada
+// Ambos strings.
 include ('funciones.php');
 
 $titulo = "Texto de ejemplo"; //(string) readline("Incluye el titulo de la pagina: ")"";
