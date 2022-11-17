8799177c2120042268c0c06f4e1b8e7ee4d736be
Iván Barrios <116113099+ibarrom693@users.noreply.github.com>
2022-11-17 19:22:08 +0100

    Add files via upload

---
+++ tema3/ejercicio11.php
@@ -0,0 +1,9 @@
+<?php
+
+include('funciones.php');
+$arreglo["perro"] = "Mamífero doméstico de la familia de los cánidos, de tamaño, forma y pelaje muy diversos";
+//$arreglo["gato"] = "Mamífero doméstico de la familia de los gatos";
+$titulo=2;
+
+ejercicio11($arreglo,$titulo);
+?>
