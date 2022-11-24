cb7c19ad201fd77acc32717ea2c02de5d173b0ca
fpertru695 <118285546+fpertru695@users.noreply.github.com>
2022-11-24 18:55:42 +0100

    Update readme.md

--- tema3/ejercicio2.php
+++ tema3/ejercicio2.php
@@ -1,6 +1,9 @@
 <?php
 include ('funciones.php');
 
+//Ejercicio 2. Entrada una cadena con el texto y un carácter que indica la modificación del texto.
+// Cadena : string.
+// Logico: string.
 $cadena = "Texto de prueba";
 
 echo "<p>¿Cómo desea el texto?<p>";
@@ -11,6 +14,7 @@ echo "<p> - subrayado (s) <p>";
 
 $logico = "n";
 
+//Función salida: string
 echo cursiva($cadena, $logico);
 
 ?>
