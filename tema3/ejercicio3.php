8a22e3a36874f2f7a6fd8d1e7eae3dbcaeb4f01c
fpertru695 <118285546+fpertru695@users.noreply.github.com>
2022-11-24 18:57:24 +0100

    Update readme.md

--- tema3/ejercicio3.php
+++ tema3/ejercicio3.php
@@ -1,8 +1,12 @@
 <?php
 include ('funciones.php'); //añadido por Pablo
 
+// Ejercicio 3. Una cadena de texto y un número que indica el heading.
+// Num: int que da el tamaño.
+// Cad: string
 $num = 2;
 $cad = "Titulo $num";
 
+//Función de salida que imprime el título.
 titulo($cad, $num);
 ?>
